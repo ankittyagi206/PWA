404f383baa6a4abb86e2bc0375cb568f456d56ee
Ankit Tyagi <ankit.tyagi02@telusinternational.com>
2023-10-18 23:25:35 +0530

    pwa camera

--- sw.js
+++ sw.js
@@ -2,16 +2,15 @@ self.addEventListener("install", (e) => {
   e.waitUntil(
     caches
       .open("static")
-      .then((cache) =>
-        cache.addAll(["./", "./images/logo192.png", "./src/style.css"])
+      ?.then((cache) =>
+        cache.addAll(["./images/logo192.png", "./src/style.css"])
       )
   );
 });
 
 self.addEventListener("fetch", (e) => {
   console.log("fetch now", e.request.url);
-
-  e.respondWith(caches.match(e.request)).then((response) => {
+  e.respondWith(caches.match(e.request))?.then((response) => {
     return response || fetch(e.request);
   });
 });
