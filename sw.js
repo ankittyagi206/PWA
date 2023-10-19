e7fe89d6d8679cac109f54a30c15f8cadb7966e7
Ankit Tyagi <ankit.tyagi02@telusinternational.com>
2023-10-20 01:42:57 +0530

    fix: build

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener("install", (e) => {
   e.waitUntil(
     caches
       .open("static")
-      ?.then((cache) => cache.addAll([".", "./images", "style.css"]))
+      ?.then((cache) => cache.addAll([".", "index.html", "style.css"]))
   );
 });
 
